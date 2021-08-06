d3794b7c4ff91030b477c709c27096cd2ce4c5fa
DIALLO Thierno Mamadou <thiernomdou63@gmail.com>
2021-08-06 08:46:58 +0200

    restaurant_card

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <RestaurantCard></RestaurantCard>
+  </div>
+</template>
+
+<script>
+import RestaurantCard from '@/components/RestaurantCard.vue'
+
+export default {
+  name: 'App',
+  components: {
+    RestaurantCard
+  }
+  
+}
+</script>
+
+<style>
+@import url('/style/rest.css');
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap');
+#app {
+  font-family: 'Roboto, sans-serif';
+}
+</style>
